cf9b1a694f3842916320014e99879c7002da3ea6
Bustopher <chrissolanilla@knights.ucf.edu>
2022-12-14 15:37:36 -0500

    What I did so far was added a sum varible and actually called the function. I wrote down a lot of comments for what I want to do in the future, and will plan to do those later. I think looping through 100 emails at a time will be tricky but I think passing in a sum varible in the function will make it easier or possible. The time complexity of this program will probably end up being O(N^2) since we are looping our function inside another for loop for us to loop through all our emails.

--- project/hello.py
+++ project/hello.py
@@ -13,10 +13,13 @@ email_sender=""
 password= ""
 #subject var
 subject="Subscription activated"
+#sum varible so we can send 100 emails at a time
+sum=0
 #function that sends the email
 def sendEmail():
     with open(r'C:\Users\chris\OneDrive\Documents\PythonScrape\project\emails.csv.csv','r') as csvfile:
         reader=csv.reader(csvfile)
+        #I need to make sure it only sends up to 100 emails. after that I need to make sure when its called again it picks up where it left off. 
         for line in reader:
             text= "hello " + line[1] +" your "+line[2]+" plan has activated"
             #(test our subject message) 
@@ -39,6 +42,24 @@ def sendEmail():
 email_sender=input(print("what is your email address\n"))
 
 password= input(print('enter your password'))
+#now we have to actually call the function but within some loop for how many email address we have. 
+#I think one solution is to have another csv file of all our email addresses with their passwords and read the varible from there as well.
+
+#open csv file with our email addresses and passwords to send emails
+with open('emailsenders.csv','r') as csvfile:
+    reader2= csv.reader(csvfile)
+    #loop for how many emails we have in the csv file
+    for col in reader2:
+        #setting the email and password to what is in the csv file
+        email_sender=col[0]
+        password=col[1]
+        #now we call the function passing in the varibles?
+        #create a sum varible that increments by 100 so that we can pass it in the function so it can loop from there to there
+        sum+=100
+        sendEmail()
+        # i guess we dont pass in varibles cause it has no parameters lol
+        #perhaps we should pass in varibles in the parameter containing the email server and stuff. 
+
 
 
 
